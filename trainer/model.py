5c3b5e41cde1c63185f40529b36a1103d7c33683
andydevs <akanshul97@gmail.com>
2021-04-07 10:20:20 -0400

    Nevermind

--- trainer/model.py
+++ trainer/model.py
@@ -4,8 +4,6 @@ Define model function
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout
 
-# Global dropout rate
-DROPOUT_RATE = 0.2
 
 def create_model(vocab_size, embedding_units, lstm_units, dense_units):
     """
@@ -13,11 +11,8 @@ def create_model(vocab_size, embedding_units, lstm_units, dense_units):
     """
     model = Sequential([
         Embedding(vocab_size, embedding_units),
-        Dropout(DROPOUT_RATE),
-        LSTM(lstm_units, recurrent_dropout=DROPOUT_RATE),
-        Dropout(DROPOUT_RATE),
+        LSTM(lstm_units),
         Dense(dense_units, activation='relu'),
-        Dropout(DROPOUT_RATE),
         Dense(vocab_size, activation='softmax')
     ])
     model.compile(
